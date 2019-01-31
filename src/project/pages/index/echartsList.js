6ad13887e8022569517e380729ec32bafe68e53f
gongyuhua <yg348@njit.edu>
2019-01-30 21:18:07 -0500

    Use Echarts to create three charts in home page.

---
+++ src/project/pages/index/echartsList.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import ReactEcharts from "echarts-for-react";
+
+class EchartList extends Component {
+  render() {
+    var option = {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          data: this.props.data,
+          type: this.props.type
+        }
+      ]
+    };
+
+    return (
+      <ReactEcharts
+        title="Weekly Data"
+        option={option}
+        style={{ width: "100%", height: "300px" }}
+      />
+    );
+  }
+}
+
+export default EchartList;
--- src/project/pages/index/index.js
+++ src/project/pages/index/index.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
+import { Row, Col } from "antd";
+import EchartList from "./echartsList";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      echartsData: [820, 932, 901, 934, 1290, 1330, 1320]
+    };
+  }
   render() {
-    return <div>homepage</div>;
+    return (
+      <div>
+        <Row>
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <h1>Bar Charts</h1>
+            <EchartList data={this.state.echartsData} type="bar" />
+          </Col>
+
+          <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <h1>Line Charts</h1>
+            <EchartList data={this.state.echartsData} type="line" />
+          </Col>
+
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <h1>Scatter</h1>
+            <EchartList data={this.state.echartsData} type="scatter" />
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
 
