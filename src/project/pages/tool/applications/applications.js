1aad15e9d61116f0f24d9c3ea35389ceb35624da
gongyuhua <yg348@njit.edu>
2019-02-01 17:02:56 -0500

    Add health tracker for application page.

--- src/project/pages/tool/applications/applications.js
+++ src/project/pages/tool/applications/applications.js
@@ -3,6 +3,7 @@ import "antd/dist/antd.css";
 import { Tabs } from "antd";
 import Salary from "./salary";
 import Rent from "./rent";
+import Bmi from "./health";
 const TabPane = Tabs.TabPane;
 
 class Application extends Component {
@@ -17,7 +18,7 @@ class Application extends Component {
             <Rent />
           </TabPane>
           <TabPane tab="Health" key="3">
-            Content of Tab Pane 3
+            <Bmi />
           </TabPane>
           <TabPane tab="Age Calculator" key="4">
             Content of Tab Pane 4
---
+++ src/project/pages/tool/applications/editor.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+
+class Health extends Component {
+  render() {
+    return <div>Health</div>;
+  }
+}
+
+export default Health;
