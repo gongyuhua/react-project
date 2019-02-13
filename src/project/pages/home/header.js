7cb7281fe0b2988d6862461307188a54fda6f641
gongyuhua <yg348@njit.edu>
2019-02-12 22:42:45 -0500

    Add Sign out link for header on the homepage

--- src/project/pages/home/header.js
+++ src/project/pages/home/header.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import "antd/dist/antd.css";
-import { Layout, Icon } from "antd";
+import { Layout, Menu, Icon } from "antd";
 import "./header.scss";
 const { Header } = Layout;
+const SubMenu = Menu.SubMenu;
 
 class Top extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: sessionStorage.getItem("name") || ""
+    };
+  }
+  componentDidMount() {
+    console.log(this.props.history);
+  }
+  goBack() {
+    this.props.history.push("/login");
+  }
   render() {
     return (
       <Header style={{ background: "#fff" }}>
@@ -13,6 +25,19 @@ class Top extends Component {
           type={this.props.collapsed ? "menu-unfold" : "menu-fold"}
           onClick={this.props.toggle}
         />
+        <Menu mode="horizontal" className="logOut">
+          <SubMenu
+            title={
+              <span className="submenu-title-wrapper">
+                {this.state.userName}
+              </span>
+            }
+          >
+            <Menu.Item key="alipay">
+              <a onClick={() => this.goBack()}>Sign Out</a>
+            </Menu.Item>
+          </SubMenu>
+        </Menu>
       </Header>
     );
   }
